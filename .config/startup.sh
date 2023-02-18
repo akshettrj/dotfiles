c8f08eb20018443b592c33c4bda68ad0747d8e35
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2023-02-18 13:07:10 +0530

    Start using awesomewm

--- .config/startup.sh
+++ .config/startup.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 
-( xrandr | grep "HDMI-0 connected" ) && HDMI_CONNECTED=true || HDMI_CONNECTED=false
+( xrandr | grep --perl-regexp 'HDMI-\d connected' ) && HDMI_CONNECTED=true || HDMI_CONNECTED=false
 
 if $HDMI_CONNECTED; then
     _layout_samsung_main.sh
     _xorg_nvidia_force_composition.sh
 fi
 
+# ( pidof dunst || dunst 2>/dev/null >/dev/null ) & disown
+# ( bash "$HOME/.config/polybar/launch_polybar.sh" ) & disown
+( pidof -x sxhkd || sxhkd 2>/dev/null >/dev/null ) & disown
+( pidof flameshot || flameshot 2>/dev/null >/dev/null ) & disown
+( pidof mpd || mpd 2>/dev/null >/dev/null ) & disown
+( pidof mpDris2 || mpDris2 2>/dev/null >/dev/null ) & disown
+( killall nm-applet; nm-applet 2>/dev/null >/dev/null ) & disown
+( killall blueman-applet; blueman-applet 2>/dev/null >/dev/null ) & disown
+( pidof picom || picom 2>/dev/null >/dev/null ) & disown
+( killall copyq; sleep 10 && copyq 2>/dev/null >/dev/null ) & disown
 
-_check_and_disown() {
-    ( pidof "$1" || "$1" >/dev/null 2>&1 ) & disown
-}
-
-_sleep_and_disown() {
-    ( killall "$1"; sleep 10 && "$1" >/dev/null 2>&1 ) & disown
-}
-
-_check_and_disown dunst
-_check_and_disown flameshot
-_check_and_disown mpd
-_check_and_disown mpDris2
-_check_and_disown nm-applet
-_check_and_disown blueman-applet
-_check_and_disown picom
-
-_sleep_and_disown copyq
-
-xwallpaper --zoom "$HOME/media/wallpapers/power_red.jpg" &
+xwallpaper --zoom "$HOME/media/wallpapers/panda-2-1920×1080.jpg" &
 
 wmname LG3D &
