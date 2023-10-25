b4ea57de850ae23014e3b0699cc2a84aea516a7c
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2023-10-25 18:53:55 +0530

    update config and packages list

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,7 +12,7 @@ config.font = wezterm.font_with_fallback({
     "Lohit Hindi",
 })
 config.color_scheme = 'Gruvbox Dark (Gogh)'
-config.hide_tab_bar_if_only_one_tab = false
+config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.95
 config.audible_bell = "Disabled"
 
