3683bddd26e1de6868f0cedd6a463bc4626534d9
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2023-10-29 20:15:53 +0530

    [waybar] support for custom separator fontsize

--- .config/waybar/scripts/run_waybar.sh
+++ .config/waybar/scripts/run_waybar.sh
@@ -4,15 +4,19 @@ _HOSTNAME="$(hostnamectl --static)"
 
 if [ "${_HOSTNAME}" = "akkihp" ]; then
 
-  _WAYBAR_FONT_SIZE=12px
+  _WAYBAR_FONT_SIZE=14px
+  _WAYBAR_SEPARATOR_FONT_SIZE=28px
 
 elif [ "${_HOSTNAME}" = "ltrcakki" ]; then
 
   _WAYBAR_FONT_SIZE=10px
+  _WAYBAR_SEPARATOR_FONT_SIZE=18px
 
 fi
 
-_WAYBAR_FONT_SIZE=${_WAYBAR_FONT_SIZE:-12px} envsubst < ~/.config/waybar/style.css > /tmp/waybar_style.css
+_WAYBAR_FONT_SIZE=${_WAYBAR_FONT_SIZE:-12px} \
+_WAYBAR_SEPARATOR_FONT_SIZE=${_WAYBAR_SEPARATOR_FONT_SIZE:-20px} \
+   envsubst < ~/.config/waybar/style.css > /tmp/waybar_style.css
 
 pidof waybar && killall -9 waybar
 waybar -c ~/.config/waybar/config.jsonc -s "/tmp/waybar_style.css" &
