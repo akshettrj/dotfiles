a7a6a3056f5d13f8a24f773aac21c208d9b8e438
Akshett Rai Jindal <akshettrai.jindal@sprinklr.com>
2025-03-04 11:38:45 +0530

    updates

--- .config/zsh/source/venv_auto_load.zsh
+++ .config/zsh/source/venv_auto_load.zsh
@@ -1,23 +1,19 @@
-function cd() {
-  builtin cd "$@"
-  check_and_load_venv
-}
-
 function check_and_load_venv() {
   if [[ -z "$VIRTUAL_ENV" ]] ; then
     ## If venv folder is found then activate the vitualenv
-      if [[ -d ./venv ]] ; then
+      if [[ -d ./venv ]]; then
         source ./venv/bin/activate
+      elif [[ -d ./.venv ]]; then
+        source ./.venv/bin/activate
       fi
   else
-    ## check the current folder belong to earlier VIRTUAL_ENV folder
-    # if yes then do nothing
-    # else deactivate
       parentdir="$(dirname "$VIRTUAL_ENV")"
       if [[ "$PWD"/ != "$parentdir"/* ]] ; then
         deactivate
       fi
   fi
 }
+
+chpwd_functions=(${chpwd_functions[@]} check_and_load_venv)
 
 check_and_load_venv
